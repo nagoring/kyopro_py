61cb099543cce088dcc3975e70b4170af6cf5995
nagomi <nagomi.github@gmail.com>
2024-06-05 13:49:41 +0900

    Add 	A - Subsegment Reverse!

---
+++ abc356_a.py
@@ -0,0 +1,29 @@
+def run():
+    N, L, R = map(int, input().split(" "))
+    # N, L, R = map(int, "10 1 10".split(" "))
+    all_array = list(range(1, N + 1))
+    reverse_array = list(range(L, R + 1))
+    reverse_array.reverse()
+    begin_index = 0
+    end_index = 0
+    for i in all_array:
+        if i == L:
+            begin_index = L
+        if i == R:
+            end_index = R
+
+    output_array = all_array
+    reverse_index = 0
+
+    for i in range(1, N + 1):
+        if begin_index <= i <= end_index:
+            output_array[i - 1] = reverse_array[reverse_index]
+            reverse_index += 1
+            continue
+
+    output = " ".join(map(str, output_array))
+    print(output)
+
+
+if __name__ == '__main__':
+    run()
