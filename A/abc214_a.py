594822f7e6d11eb556e94cc7758d74722d6bb2d0
nagomi <nagomi.github@gmail.com>
2024-07-19 17:46:40 +0900

    A - New Generation ABC

---
+++ A/abc214_a.py
@@ -0,0 +1,12 @@
+def run():
+    N = int(input())
+    if 1 <= N <= 125:
+        print(4)
+    elif 126 <= N <= 211:
+        print(6)
+    elif 212 <= N <= 214:
+        print(8)
+
+
+if __name__ == '__main__':
+    run()
