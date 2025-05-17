bed1afbdb04ee575128752cdd34e64dcbea50381
nagomi <nagomi.github@gmail.com>
2025-05-17 18:09:49 +0900

    abc360_a

--- A/abc360_a.py
+++ A/abc360_a.py
@@ -1,11 +1,7 @@
 def run():
     s = input()
-    first = s[0]
-    second = s[1]
-    third = s[2]
     r_index = 0
     m_index = 0
-    s_index = 0
     index = 0
     for c in s:
         index = index + 1
@@ -13,8 +9,6 @@ def run():
             r_index = index
         if c == "M":
             m_index = index
-        if c == "S":
-            s_index = index
 
 
     if r_index < m_index:
